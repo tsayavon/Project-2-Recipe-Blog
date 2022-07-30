84e92af783c11a0657618e9fb7295a7d152fa05f
tsayavon <sayavongtiffany05@gmail.com>
2022-07-30 14:08:12 -0400

    mongodb connected

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -1,4 +1,5 @@
-
+require('../models/database');
+const Category = require('../models/Category');
 
 
 
@@ -11,3 +12,43 @@
 exports.homepage = async(req, res) => {
     res.render('index', { title: 'Recipe Blog - Homepage' });
 }
+
+
+
+
+
+
+async function insertDymmyCategoryData(){
+    try {
+        await Category.insertMany([
+          {
+            "name": "Thai",
+            "image": "thai-food.jpg"
+            },
+            {
+            "name": "American",
+            "image": "american-food.jpg"
+            }, 
+            {
+            "name": "Chinese",
+            "image": "chinese-food.jpg"
+            },
+            {
+            "name": "Mexican",
+            "image": "mexican-food.jpg"
+            }, 
+            {
+            "name": "Indian",
+            "image": "indian-food.jpg"
+            },
+            {
+            "name": "Spanish",
+            "image": "spanish-food.jpg"
+           }                
+        ]);
+    } catch (error) {
+        console.log('err', + error)
+    }
+}
+
+insertDymmyCategoryData();
--- server/models/database.js
+++ server/models/database.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParaser: true, useUnifiedTechnology: true });
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-    console.log('Connected')
+db.once('open', function(){
+  console.log('Connected')
 });
 
 // Models
