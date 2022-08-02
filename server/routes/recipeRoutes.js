aac026df55b4f8814db6ab9c2c85508d0819d5b2
tsayavon <sayavongtiffany05@gmail.com>
2022-08-02 19:28:39 -0400

    search,explore,random routes

--- server/routes/recipeRoutes.js
+++ server/routes/recipeRoutes.js
@@ -10,6 +10,8 @@ router.get('/recipe/:id', recipeController.exploreRecipe);
 router.get('/categories', recipeController.exploreCategories);
 router.get('/categories/:id', recipeController.exploreCategoriesById);
 router.post('/search', recipeController.searchRecipe);
+router.get('/explore-latest', recipeController.exploreLatest);
+router.get('/explore-random', recipeController.exploreRandom);
 
 
 
